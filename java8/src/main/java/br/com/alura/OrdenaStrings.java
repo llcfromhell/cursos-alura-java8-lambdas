8bd12cd4e8ab2d998db5e7b76ae06b964602dc0f
llcfromhell <llc.from.hell@gmail.com>
2016-10-06 00:04:32 -0300

    aula -2

--- java8/src/main/java/br/com/alura/OrdenaStrings.java
+++ java8/src/main/java/br/com/alura/OrdenaStrings.java
@@ -15,15 +15,34 @@ public class OrdenaStrings {
 		palavras.add("casa do código");
 		palavras.add("caelum");
 
+		System.out.println(palavras);
+		
 		// java 8 - nova maneira de ordernar
 		palavras.sort(new ComparadorDeStringPorTamanho());
+
+		// java 8 - utilizando um comparator anonimo e lambda
+		palavras.sort((s1, s2) -> Integer.compare(s1.length() , s2.length()) );
 		
 		// java 8 - nova maneira de iterar
 		// passa uma função para consumir cada elemento
 		palavras.forEach(new ConsumidorDeString());
-		
-		System.out.println(palavras);
 
+		// criando um consumidor anonimo
+		palavras.forEach( new Consumer<String>() {
+			public void accept(String t) {
+				System.out.println(t);
+			}
+		});
+		
+		// java 8 - como a interface tem apenas 1 método
+		// podemos retirar o construir pois o compilador
+		// vai inferir este, assim como o Tipo do
+		// do parametro e deixar apenas a chamada da
+		// instrução
+		palavras.forEach( s -> System.out.println(s) );
+		
+		new Thread(() -> System.out.println("Executando um Runnable")).start();
+		
 	}
 
 }
